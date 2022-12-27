2f71b7e72524f17f709f978b2a5820f00257d880
MOHAKDev <husnain.jsdev@gmail.com>
2022-12-27 15:27:48 +0500

    push test

---
+++ src/assets/colorPalette.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  main: {
+    red: '#FF7B7B',
+  },
+  text: {
+    limeGreen: "#1AEBB7",
+    bluePrimary: "#366589",
+    baliHai: '#88A2B7',
+  },
+  bg: {
+    primaryGray: "#f4f6f8",
+  },
+};
